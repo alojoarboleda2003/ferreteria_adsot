f0ad99a30104bca5e152b868f1a22a732381af69
alojoarboleda2003 <161771718+alojoarboleda2003@users.noreply.github.com>
2025-03-17 11:12:29 -0500

    Add files via upload

---
+++ conexionbd/ConexionBD.java
@@ -0,0 +1,22 @@
+package conexionbd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexionBD
+{
+    public Connection getConnection()
+    {
+        Connection con = null;
+
+        try
+        {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ferreteria","root", "");
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
